53513e42d4f85d87560aaab608bac0f6d8c612bc
Brian Kim <brkim1324@gmail.com>
2018-11-13 08:17:24 -0500

    Added Mouse enemy object. Added Mouse Script.

---
+++ Assets/scripts/EnemyMouse.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMouse : MonoBehaviour
+{
+
+
+	public Transform Player;
+
+	public Transform Mouse;
+	public Transform idlePosition;
+	
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Vector3.Distance(Player.position, Mouse.position) <= 5.0f)
+		{
+			transform.position = Vector3.MoveTowards(transform.position, Player.position, Time.deltaTime);
+		}
+		else
+		{
+			transform.position = Vector3.MoveTowards(transform.position, idlePosition.position, Time.deltaTime);
+		}
+	}
+
+	private void OnCollisionEnter(Collision Player)
+	{
+		
+	}
+}
